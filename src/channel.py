a697e6344700f0fe7c62df887fd235e0c8e7fc53
nuklotron <vas.ledkov@gmail.com>
2023-04-19 19:11:03 +0300

    hw-1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,23 @@
+import json
+import os
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+
+    def printj(self: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(self, indent=2, ensure_ascii=False))
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_info = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        printj = json.dumps(channel_info, indent=2, ensure_ascii=False)
+        return print(printj)
