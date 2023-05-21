6a94e5ffd42f624c12f0237d003cb4901eb0c2da
nuklotron <vas.ledkov@gmail.com>
2023-05-21 21:29:06 +0300

    hw-6

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 
 class Video:
@@ -8,12 +9,30 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, id_video):
-        self.__id_video = id_video
-        self.video_info = self.youtube.videos().list(id=self.__id_video, part='snippet,statistics').execute()
-        self.title = self.video_info["items"][0]["snippet"]["title"]
-        self.link = f'https://youtu.be/{self.__id_video}'
-        self.views = self.video_info["items"][0]["statistics"]["viewCount"]
-        self.likes = self.video_info["items"][0]["statistics"]["likeCount"]
+
+        try:
+            self.__id_video = id_video
+            self.video_info = self.youtube.videos().list(id=self.__id_video, part='snippet,statistics').execute()
+
+            if self.video_info['items']:
+                self.title = self.video_info["items"][0]["snippet"]["title"]
+                self.link = f'https://youtu.be/{self.__id_video}'
+                self.views = self.video_info["items"][0]["statistics"]["viewCount"]
+                self.likes = self.video_info["items"][0]["statistics"]["likeCount"]
+
+            else:
+                self.id_video = id_video
+                self.title = None
+                self.likes = None
+                self.views = None
+                self.link = None
+
+        except HttpError:
+            self.id_video = id_video
+            self.title = None
+            self.likes = None
+            self.views = None
+            self.link = None
 
     def __str__(self):
         return self.title
